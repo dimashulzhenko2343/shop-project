3b5d676a507bc4bcebf3061abbdbeca06a36b606
dimaShulzhenko <dimashulzhenko2343@gmail.com>
2023-04-13 21:01:14 +0300

    Добавить карточки. Добавить  данные API.

---
+++ src/components/Preloader.jsx
@@ -0,0 +1,11 @@
+import './Preloader.css'
+
+function Preloader() {
+  return (
+    <div className="progress indent">
+      <div className="indeterminate"></div>
+    </div>
+  );
+}
+
+export default Preloader;
